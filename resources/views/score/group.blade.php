c778159d3282ecedb90626bf90f6de07f9f109c6
Advaith3600 <advaitharunjeena@gmail.com>
2017-02-28 11:06:05 +0530

    Score department view updated

--- resources/views/score/group.blade.php
+++ resources/views/score/group.blade.php
@@ -15,28 +15,72 @@
         <div class="col-sm-8 col-sm-offset-2">
             <h1 class="text-center">Department Score</h1><hr>
             <div class="col-sm-12 w3-margin-top">
-                <table class="table table-bordered advaith-table">
-                    <thead>
-                        <tr>
-                            <th>Name</th>
-                            <th>Chest No:</th>
-                            <th>Batch</th>
-                            <th>Roll No:</th>
-                            <th>Score</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($registers as $register)
-                            <tr class="ad">
-                                <td>{{ $register->name }}</td>
-                                <td>{{ $register->chest_no }}</td>
-                                <td>{{ $register->batch }}</td>
-                                <td>{{ $register->roll_no }}</td>
-                                <td>{{ $register->score }}</td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+                <div>
+                    <ul class="nav nav-tabs" role="tablist">
+                        <li role="presentation" class="active"><a href="#male" aria-controls="male" role="tab" data-toggle="tab">Male</a></li>
+                        <li role="presentation"><a href="#female" aria-controls="female" role="tab" data-toggle="tab">Female</a></li>
+                    </ul>
+                    <div class="tab-content">
+                        <div role="tabpanel" class="tab-pane active" id="male">
+                            <table class="table table-bordered advaith-table">
+                                <thead>
+                                    <tr>
+                                        <th>Name</th>
+                                        <th>Chest No:</th>
+                                        <th>Batch</th>
+                                        <th>Roll No:</th>
+                                        <th>Score</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($registers->where('gender', '=', 'male') as $register)
+                                        <tr class="ad">
+                                            <td>{{ $register->name }}</td>
+                                            <td>{{ $register->chest_no }}</td>
+                                            <td>{{ $register->batch }}</td>
+                                            <td>{{ $register->roll_no }}</td>
+                                            <td>{{ $register->score }}</td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                                <tfoot>
+                                    <tr>
+                                        <th colspan="5" class="text-right">Department Score: {{ $department->score }}</th>
+                                    </tr>
+                                </tfoot>
+                            </table>
+                        </div>
+                        <div role="tabpanel" class="tab-pane" id="female">
+                            <table class="table table-bordered advaith-table">
+                                <thead>
+                                    <tr>
+                                        <th>Name</th>
+                                        <th>Chest No:</th>
+                                        <th>Batch</th>
+                                        <th>Roll No:</th>
+                                        <th>Score</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($registers->where('gender', '=', 'female') as $register)
+                                        <tr class="ad">
+                                            <td>{{ $register->name }}</td>
+                                            <td>{{ $register->chest_no }}</td>
+                                            <td>{{ $register->batch }}</td>
+                                            <td>{{ $register->roll_no }}</td>
+                                            <td>{{ $register->score }}</td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                                <tfoot>
+                                    <tr>
+                                        <th colspan="5" class="text-right">Department Score: {{ $department->score }}</th>
+                                    </tr>
+                                </tfoot>
+                            </table>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
